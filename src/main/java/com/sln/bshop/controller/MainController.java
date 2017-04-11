a7aa5a1e28841f6e1ff08fd1f1d20c49c7c80a21
Randle McMurphy <jmarton2000@gmail.com>
2017-04-11 15:58:45 +0300

    Enabled CSRF protection

--- src/main/java/com/sln/bshop/controller/MainController.java
+++ src/main/java/com/sln/bshop/controller/MainController.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
